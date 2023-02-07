e133b37d285fa12d9f3c265764a9da477d705481
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-07 07:45:40 -0500

    part5

---
+++ taller4.py
@@ -0,0 +1,2 @@
+""" 4. Que son las expresiones regulares en Python? """
+print("Son los siguientes: . ^ $ * + ? { } [ ] \ | ( ); y sirven para hacer coincidir combinaciones de caracteres que estan en cadenas mediante patrones")

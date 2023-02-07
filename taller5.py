4542dafa81178c15fe99654b8f8c1bb1569bc795
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-07 07:55:09 -0500

    part6

---
+++ taller5.py
@@ -0,0 +1,8 @@
+""" 5. Enumere 5 tipos de datos en Python y suministre un valor de
+ejemplo de cada uno. """
+
+Nombre = Jesus
+edad = 17
+vivo_yViudo= True
+hotel = ["trivago", "transilvania"]
+fechasHistoricas = ("11/09/2001, 01/09/1939, 08/09/2022")

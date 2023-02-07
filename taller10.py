0561febe73feb524db9f72a7916eda4a9abfa8f4
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-07 09:26:16 -0500

    part10

---
+++ taller10.py
@@ -0,0 +1,19 @@
+
+mesConsumo ="Enero"
+valorKw= 315.6
+TotalKw= 29250
+estrato="1"
+
+consumoTotal= TotalKw*valorKw 
+if estrato==1:
+    descuento=consumoTotal*0.6
+if estrato==2:
+    descuento=consumoTotal*0.5
+if estrato==3:
+    descuento=consumoTotal*0.15
+
+
+consumoTotal1= consumoTotal*0.6
+
+
+print("El valor a total a pagar durante el mes de" + mesConsumo + "es de" + consumoTotal1 + "en pesos Colombianos (COP)")

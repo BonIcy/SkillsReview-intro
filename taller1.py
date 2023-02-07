dc2fe7103a94d5f08c9c5e8987d6f7d80569089a
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-07 07:19:57 -0500

    ejericio1.1

---
+++ taller1.py
@@ -0,0 +1,10 @@
+""" 1. Qué operadores utiliza Python en los siguientes casos:
+
+A. División Modular
+B. Exponenciación
+C. División que retorne entero. """
+
+num1 = 2
+num2 = 3
+
+print("A.División modular = %")

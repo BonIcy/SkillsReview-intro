19e72ccccc7b810f1282b33164b4b4a850feb2c0
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-07 07:27:16 -0500

    taller1

--- taller1.py
+++ taller1.py
@@ -7,4 +7,8 @@ C. División que retorne entero. """
 num1 = 2
 num2 = 3
 
-print("A.División modular = %")
+print("A.División modular = %", (num1)%(num2) = (num1%num2))
+print("B. Exponenciación = **", (num1)**(num2) = (num1**num2))
+print("C. División que retorne entero. = //", (num1)//(num2) = (num1//num2))
+
+

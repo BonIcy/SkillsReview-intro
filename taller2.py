dd9920248a03cfd5a517cbaf868ede76d94529aa
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-07 07:37:30 -0500

    taller2

---
+++ taller2.py
@@ -0,0 +1,5 @@
+""" 2. En la jerarquía de operadores, cuáles son los que más
+prioridad tienen cuando el intérprete de Python los evalúa?
+ """
+
+print("Los parentesis tienen mayor valor(), seguidos por la potencia **, luego están los multiplicadores *, y todos los divisores *,/,%,//; posteriormente quedan los relacionales <,>,!=(etc); y por ultimo los logicos And y Or")

0999148a349d9d5bfbf20184023ada9b7c3b33c9
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-07 08:04:20 -0500

    part7

--- taller5.py
+++ taller5.py
@@ -6,3 +6,5 @@ edad = 17
 vivo_yViudo= True
 hotel = ["trivago", "transilvania"]
 fechasHistoricas = ("11/09/2001, 01/09/1939, 08/09/2022")
+
+print(type(Nombre), type(edad), type(vivo_yViudo) type(hotel), type(fechasHistoricas))
---
+++ taller6.py
@@ -0,0 +1,5 @@
+""" 6. En sus propias palabras, qué son las funciones
+preconstruidas y proporcione 2 ejemplos. """
+
+print("Son las que vienen ya construidas y definidas por defecto, asi nos ahorran tiempo a la hora de escribir codigo:D. Un par de ejemplos serian el print(), que es el que estoy usando para mostrar esto, y el tuple(), al escribir letras de estas funciones con el tabulador podemos hacer una selección rapida.| PD: vivan las predefinidas")
+

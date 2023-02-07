7732d768228b5e45366f4835e5e075f999ea0d7f
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-07 07:39:50 -0500

    part3

--- taller2.py
+++ taller2.py
@@ -3,3 +3,4 @@ prioridad tienen cuando el intérprete de Python los evalúa?
  """
 
 print("Los parentesis tienen mayor valor(), seguidos por la potencia **, luego están los multiplicadores *, y todos los divisores *,/,%,//; posteriormente quedan los relacionales <,>,!=(etc); y por ultimo los logicos And y Or")
+print(4+2*9(8-1)/2)
---
+++ taller3.py
@@ -0,0 +1,7 @@
+""" 3. Si hay operadores de igual precedencia, en qué orden se
+ejecutan?
+
+A. De izquierda a derecha
+B. De derecha a izquierda """
+
+p

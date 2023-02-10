16726192b63dad79e076625e452ebb9ade7de287
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-10 13:26:25 -0500

    feat(root):Impresiones

--- taller6.py
+++ taller6.py
@@ -3,3 +3,4 @@ preconstruidas y proporcione 2 ejemplos. """
 
 print("Son las que vienen ya construidas y definidas por defecto, asi nos ahorran tiempo a la hora de escribir codigo:D. Un par de ejemplos serian el print(), que es el que estoy usando para mostrar esto, y el tuple(), al escribir letras de estas funciones con el tabulador podemos hacer una selección rapida.| PD: vivan las predefinidas")
 
+""" no se si haya que poner ejemplo aqui """

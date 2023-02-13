d91bdee0dfe817bd1f4048fe0e74671735b34560
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-02-13 07:51:04 -0500

    fix(root): taller9edit

--- taller9.py
+++ taller9.py
@@ -1,18 +1,14 @@
-""" 9. Construya un algoritmo en Python, que permita ingresar la
+""" " 9. Construya un algoritmo en Python, que permita ingresar la
 información de un empleado e imprima el nombre, los
 apellidos y la antigüedad. Los datos que se deben solicitar
-son los siguientes:
+son los siguientes: 
 *Nombre * Teléfono *Año de ingreso a la empresa
 *Apellidos *Edad. """
 
-""" nombre = str(imput("Coloca tu nombre"))
-apellidos = str(imput("Coloca tus apellidos"))
-edad = str(imput("¿Cual es tu edad(Pon solo el numero)?"))
-telefono = str(input("Pon tu número de telefono celular o fijo"))
-añoDeIngreso = str(input("Escribe el año en que ingresaste a la empresa"))
+nombre = input("Coloca tu nombre:")
+apellidos =input("Coloca tus apellidos: ")
+edad = input("¿Cual es tu edad(Pon solo el numero)?: ")
+telefono = input("Pon tu número de telefono celular o fijo: " )
+añoDeIngreso = input("Escribe el año en que ingresaste a la empresa: ")
 
-print("Tu nombre completo es=", nombre + apellidos, "\n Tu año de ingreso fue en el año", + añoDeIngreso)
- """
-
-
-  """ pd: lo  comento porque está en progreso """
+print("Tu nombre completo es :", nombre,  apellidos, "y tu año de ingreso fue en el año", añoDeIngreso)
